45dff114d7cac2260f817315b4663242625e8970
FurmanVlad <vladfurman98@gmail.com>
2024-02-26 17:16:06 +0200

    Creating home page

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Welcome to Our Online Shop</h1>
+      <p>
+        Explore our wide range of products and find exactly what you're looking for!
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
