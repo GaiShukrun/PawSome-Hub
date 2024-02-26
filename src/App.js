c6217833f2c9b0d79ea4acc19a92e805017bf628
FurmanVlad <vladfurman98@gmail.com>
2024-02-26 17:15:13 +0200

    App update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React , { useState }  from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './components/SignUp/SignUpPage';
+import Login from './components/Login/LoginPage';
+import HomePage from './components/Home/HomePage';
+import GuestNavbar from './components/Navbar/GuestNavbar';
+import UserNavbar from './components/Navbar/UserNavbar';
+
+
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      {isLoggedIn ? (
+        <UserNavbar />
+      ) : (
+        <GuestNavbar />
+      )}
+        <Routes>  
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/" element={<HomePage />} />
+          {/* Other routes can be added here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
