6f2c220ea807036068a4f0b98229bad5932f692a
Gai <gaish@ac.sce.ac.il>
2024-03-10 14:22:52 +0200

    new model data to save the orders the user made

---
+++ server/models/UserFormData.js
@@ -0,0 +1,38 @@
+// UserFormData.js
+
+const mongoose = require('mongoose');
+
+const userFormDataSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  cardNumber: {
+    type: String,
+    required: true
+  },
+  expiryDate: {
+    type: String,
+    required: true
+  },
+  cvc: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  itemsCheckout: {
+    type: [String], // Array of strings for item names and quantities
+    required: true
+  }
+});
+
+const UserFormData = mongoose.model('UserFormData', userFormDataSchema);
+
+module.exports = UserFormData;
